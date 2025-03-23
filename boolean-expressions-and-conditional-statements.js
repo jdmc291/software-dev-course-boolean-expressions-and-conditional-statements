67978cf3b1645bdcc1540d3bce133cbd18ab6458
Jalen McBride <jamcbride131@gmail.com>
2025-03-23 11:01:20 -0500

    I added my own choices and story to the initial repository

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,17 +28,69 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+let hasHat;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
+
+  let answer = readline.question("You find an oddly shaped hat. Do you \"keep\" it in your backpack or do you \"leave\" it where you found it?")
+  
+
+  if (answer === "keep"){
+    hasHat = true;
+  }
+
+  else
+    hasHat = false;
+
+  const conversation = readline.question("You run into a man who wants to have a conversation with you. Do you have a \"good\" conversation or \"bad\" conversation?")
+
+  console.log("while speaking to the man. You discover that he lost a hat. The exact hat that you saw while walking through the mountain.")
+
+  if (conversation === "bad" && hasHat)
+    console.log("The man thanks you for picking up his has and walks off")
+
+  else if(conversation === "good" && hasHat)
+    console.log("The man is very happy and takes you to the secret elevator where you go to the top of the mountain. Hooray!");
+
+  else if(conversation === "good" && !hasHat)
+    console.log("You tell the man where the hat is and he goes off to get it. You continue to walk around through the mountain.");
+
+  else if(conversation=== "bad" && !hasHat)
+    console.log("Before you can tell him where is hat is, he walks away.");
+
+
+}
+
+  else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
+
 } else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
-} else {
+  const objectOfChoice = readline.questionInt("While traveling through the forest, you find a cave with iridescent light coming out of it.\n You go in to see 3 shiny objects. Which one do you choose? \n1.\tSword of Excalibur\n2.\tMagic Spellbook\n3.\tA Very Long Stick\nPick a Number: ")
+
+  console.log("You find your way to the village. A magical dragon is attacking it as you arrive");
+
+  if(objectOfChoice === 1 || objectOfChoice === 2){
+    console.log("You decide to help the village.")
+    if(objectOfChoice === 1)
+    {
+      console.log("You unsheath your sword. You feel its surging power with every step. As the flaming breath of the dragon reach you, the sword slices it with ease.\nYou slay the dragon and become the hero of the village.")
+
+    }
+    if(objectOfChoice === 2){
+      console.log("You open the book to find a \"Talk to Dragons\" spell that you cast on yourself. You manage to find high ground to talk to the dragon. Apparently the dragon was just hungry.\nThe town feeds the dragon and it becomes guardian of the village.\nYou become known as the Dragon Master.")
+    }
+
+  }
+
+  else
+  console.log("You leave the village immediately to avoid being hurt.")
+}
+
+else {
   console.log("You get lost and wander aimlessly.");
 }
 
